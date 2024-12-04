92ac54755963bb3b5c7b2acdec507b5a41391aec
Brian Murray <brian@bmurray.ca>
2024-12-04 02:55:34 -0700

    dwords for events

--- simconnect/defs.go
+++ simconnect/defs.go
@@ -64,6 +64,12 @@ const GROUP_PRIORITY_STANDARD DWORD = 1900000000       // standard priority
 const GROUP_PRIORITY_DEFAULT DWORD = 2000000000        // default priority
 const GROUP_PRIORITY_LOWEST DWORD = 4000000000         // priorities lower than this will be ignored
 
+// Event flag values
+const SIMCONNECT_EVENT_FLAG_DEFAULT DWORD = 0x00000000
+const SIMCONNECT_EVENT_FLAG_FAST_REPEAT_TIMER DWORD = 0x00000001   // set event repeat timer to simulate fast repeat
+const SIMCONNECT_EVENT_FLAG_SLOW_REPEAT_TIMER DWORD = 0x00000002   // set event repeat timer to simulate slow repeat
+const SIMCONNECT_EVENT_FLAG_GROUPID_IS_PRIORITY DWORD = 0x00000010 // interpret GroupID parameter as priority value
+
 func derefDataType(fieldType string) (DWORD, error) {
 	var dataType DWORD
 	switch fieldType {
